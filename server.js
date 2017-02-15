2dc454ba80ec90f3aec8f4e8777faa35df716a50
Zain Syed <Shaheen@Zains-iMac.home>
2017-02-15 13:14:12 +0000

    port change

--- server.js
+++ server.js
@@ -2,8 +2,15 @@ var express = require('express');
 
 // Create our app
 var app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 
+app.use(function (req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 
 app.use(express.static('public'));
 
